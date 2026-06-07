4f4cd11378f3ada6942a5245f60bb1247a4cb8cc
Arnab Ray <18110248+arnab-ray@users.noreply.github.com>
2026-06-07 22:34:20 +0530

    set up project

---
+++ transaction-routing/src/main/java/io/arnab/transactionrouting/TransactionRoutingApplication.java
@@ -0,0 +1,13 @@
+package io.arnab.transactionrouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionRoutingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionRoutingApplication.class, args);
+    }
+
+}
---
+++ transaction-routing/src/test/java/io/arnab/transactionrouting/TransactionRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.arnab.transactionrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionRoutingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
